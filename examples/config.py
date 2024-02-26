c11f3896978f9cc57d8322e2688510624db67a71
algorithmlover2016 <yubo.upt@gmail.com>
2024-02-26 14:39:38 +0800

    add a azure_openai_version for demo of How_to_call_functions_for_knowledge_retrieval

---
+++ examples/config.py
@@ -0,0 +1,10 @@
+import os
+import dotenv
+dotenv.load_dotenv() # load environment variables from .env file
+
+# Azure OpenAI
+api_key = os.getenv("OPENAI_API_KEY_GPT4")
+api_version = os.getenv("API_VERSION", "2023-07-01-preview")
+azure_endpoint = os.getenv("AZURE_ENDPOINT", "https://kslabopenai2.openai.azure.com")
+azure_embedding_model = os.getenv("AZURE_EMBEDDING_MODEL", "text-embedding-openai")
+GPT_MODEL = os.getenv("AZURE_GPT_MODEL", "gpt-4-32k")
