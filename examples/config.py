0c49216db8a7956dc038869ca729d78d17c78128
algorithmlover2016 <yubo.upt@gmail.com>
2024-02-26 14:40:52 +0800

    adjust format

--- examples/config.py
+++ examples/config.py
@@ -6,5 +6,6 @@ dotenv.load_dotenv() # load environment variables from .env file
 api_key = os.getenv("OPENAI_API_KEY_GPT4")
 api_version = os.getenv("API_VERSION", "2023-07-01-preview")
 azure_endpoint = os.getenv("AZURE_ENDPOINT", "https://kslabopenai2.openai.azure.com")
-azure_embedding_model = os.getenv("AZURE_EMBEDDING_MODEL", "text-embedding-openai")
 GPT_MODEL = os.getenv("AZURE_GPT_MODEL", "gpt-4-32k")
+
+azure_embedding_model = os.getenv("AZURE_EMBEDDING_MODEL", "text-embedding-openai")
