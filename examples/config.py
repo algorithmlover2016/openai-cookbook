a70de995776637b847d27f9af8f92ff8d7aab741
algorithmlover2016 <yubo.upt@gmail.com>
2024-02-27 10:06:16 +0800

    add default value or promt message

--- examples/config.py
+++ examples/config.py
@@ -3,7 +3,7 @@ import dotenv
 dotenv.load_dotenv() # load environment variables from .env file
 
 # Azure OpenAI
-api_key = os.getenv("OPENAI_API_KEY_GPT4")
+api_key = os.getenv("OPENAI_API_KEY_GPT4", "<your OpenAI API key if not set as env var>")
 api_version = os.getenv("API_VERSION", "2023-07-01-preview")
 azure_endpoint = os.getenv("AZURE_ENDPOINT", "https://kslabopenai2.openai.azure.com")
 GPT_MODEL = os.getenv("AZURE_GPT_MODEL", "gpt-4-32k")
